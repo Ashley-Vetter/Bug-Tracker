f9555de1e6c50fedc49f13541bc0501100850948
Ashley-Vetter <127203216+Ashley-Vetter@users.noreply.github.com>
2023-08-09 18:56:05 +0200

    Added functionality to capture data from the form. The data is stored in an object and JSON.stringify is applied I cannot figure out why input.addEventListener is coming up as null.

--- app.js
+++ app.js
@@ -1,41 +1,90 @@
-//test
-/*let something = () => {
+let issueArray = [];
 
-}
-*/
-const issueArr = [];
+document.addEventListener('DOMContentLoaded', () => {
+    const people = ['Ashley', 'Chandri', 'Kelo', 'Peter'];
 
-//creating issues
-function issue(s, d, p, da, pr, pe, st){
-    this.summary = s;
-    this.desc = d;
-    this.personID = p;
-    this.date = da;
-    this.project = pr;
-    this.personAssigned = pe;
-    this.statusOfIssue = st;
+    setupAutocomplete('discovered-by', people);
+    setupAutocomplete('assigned-to', people);
+    function setupAutocomplete(inputId, people) {
+        let input = document.getElementById(inputId);
+        const suggestionsContainer = document.createElement('ul');
+        suggestionsContainer.classList.add('suggestions');
+        document.body.appendChild(suggestionsContainer); // Append container to body once
+
+        input.addEventListener('focus', () => {
+            const value = input.value.toLowerCase();
+            const filteredPeople = people.filter(person => person.toLowerCase().includes(value));
+            displaySuggestions(filteredPeople, input);
+        });
+
+        // Clear suggestions when clicking outside the input
+        document.addEventListener('click', (event) => {
+            if (!input.contains(event.target) && !suggestionsContainer.contains(event.target)) {
+                suggestionsContainer.innerHTML = '';
+            }
+        });
+
+        input.nextElementSibling.addEventListener('click', (event) => {
+            if (event.target.tagName === 'LI') {
+                input.value = event.target.textContent;
+                suggestionsContainer.innerHTML = ''; // Clear suggestions after selecting
+            }
+        });
+
+        function displaySuggestions(filteredPeople, input) {
+            console.log('Filtered People:', filteredPeople);
+            const suggestions = filteredPeople.map(person => `<li>${person}</li>`).join('');
+            suggestionsContainer.innerHTML = suggestions;
+            if (input.value !== '' && !filteredPeople.includes(input.value)) {
+                suggestionsContainer.innerHTML += `<li>${input.value}</li>`;
+            }
+            document.body.appendChild(suggestionsContainer);
+
+            // Clear previous suggestions and append new ones
+            while (suggestionsContainer.firstChild) {
+                suggestionsContainer.removeChild(suggestionsContainer.firstChild);
+            }
+            filteredPeople.forEach(person => {
+                const suggestionElement = document.createElement('li');
+                suggestionElement.textContent = person;
+                suggestionsContainer.appendChild(suggestionElement);
+            });
+
+            console.log('Suggestions:', suggestions);
+            return suggestions;
+        }
     }
+    document.querySelector('form').addEventListener('submit', e => {
+        e.preventDefault();
+        addIssue();
+        window.location.href = "Home.html";
+    });
+});
 
+// Add issue function
+const addIssue = () => {
+    let issue = {
+        id: Date.now(),
+        summary: document.getElementById('summary').value,
+        description: document.getElementById('description').value,
+        project: document.getElementById('project').value,
+        discoveredBy: document.getElementById('discovered-by').value,
+        dateDiscovered: document.getElementById('date-discovered').value,
+        statusOfIssue: document.getElementById('status-input').value,
+        priorityLevel: document.getElementById('priority-input').value,
+        targetDate: document.getElementById('target-date').value,
+        personAssigned: document.getElementById('assigned-to').value,
+        resolutionDate: document.getElementById('res-date').value,
+        resolutionSummary: document.getElementById('res-summary').value,
+    }
+    issueArray.push(issue);
+    document.forms[0].reset();      // Clears the form for the next entries
 
-    //assigning variables to use them in a loop for the object's properties
-    let summary = prompt("Please give a summary of the issue you are having.");
-    let desc = prompt("Please provide a description of the issue.");
-    let personID = prompt("Who identified the bug?");
-    let date = prompt("What is on which you discovered the bug?");
-    let project = prompt("What project is this bug related to?");
-    let personAssigned = prompt("Please enter your name.");
-    let statusOfIssue = alert("The status of the issue is: ");
+    //Display purposes only
+    console.warn('added', { issueArray });
+    let pre = document.querySelector('#msg pre')
+    pre.textContent = '\n' + JSON.stringify(issueArray, null, 2);
 
-        let issues = new issue(`${summary}`, `${desc}`, `${personID}`, `${date}`, `${project}`, `${personAssigned}`, `${statusOfIssue}`);
-
-        issueArr.push(issues);
-
-issue();
-
-
-//assigning issues
-const defPerson = ["Ashley V.", "Chandri B.", "Kelo L.", "Peter S."];
-
-for (let is of issueArr){
-    alert(`Issue ${issueArr[is]} is assigned to ${defPerson[i]}`);
+    // save to localStorage
+    localStorage.setItem('IssueList', JSON.stringify(issueArray));
 }
--- login.js
+++ login.js
@@ -2,19 +2,18 @@ let username = "admin";
 let password = "admin";
 
 
-function HomepageRD(){
- 
-   if(document.getElementById("name").value == username && document.getElementById("pass").value == password){
-     window.location.href = "www.google.com";
-    }
+function HomepageRD() {
 
-   
+   if (document.getElementById("name").value == username && document.getElementById("pass").value == password) {
+      window.location.href = "Home/Home.html";
+   }
+
    else {
-    let form = document.getElementById("logform");
-     function handleForm(event) { event.preventDefault(); } 
-     form.addEventListener('submit', handleForm);
-    document.getElementById("invalid").innerHTML = "invalid credentials";
-    
+      let form = document.getElementById("logform");
+      function handleForm(event) { event.preventDefault(); }
+      form.addEventListener('submit', handleForm);
+      document.getElementById("invalid").innerHTML = "invalid credentials";
+
    }
 
 }
