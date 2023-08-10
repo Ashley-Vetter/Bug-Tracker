e7afb7bb81ebe135ccd4ebe951bc519719f6a213
Ashley-Vetter <127203216+Ashley-Vetter@users.noreply.github.com>
2023-08-10 11:06:55 +0200

    Stores form data and displays it on cards.

--- app.js
+++ app.js
@@ -3,32 +3,18 @@
 
 }
 */
-const issueArr = [];
+//assigning variables to use them in a loop for the object's properties
+let summary = prompt("Please give a summary of the issue you are having.");
+let desc = prompt("Please provide a description of the issue.");
+let personID = prompt("Who identified the bug?");
+let date = prompt("What is on which you discovered the bug?");
+let project = prompt("What project is this bug related to?");
+let personAssigned = prompt("Please enter your name.");
+let statusOfIssue = alert("The status of the issue is: ");
 
-//creating issues
-function issue(s, d, p, da, pr, pe, st){
-    this.summary = s;
-    this.desc = d;
-    this.personID = p;
-    this.date = da;
-    this.project = pr;
-    this.personAssigned = pe;
-    this.statusOfIssue = st;
-    }
+let issues = new issue(`${summary}`, `${desc}`, `${personID}`, `${date}`, `${project}`, `${personAssigned}`, `${statusOfIssue}`);
 
 
-    //assigning variables to use them in a loop for the object's properties
-    let summary = prompt("Please give a summary of the issue you are having.");
-    let desc = prompt("Please provide a description of the issue.");
-    let personID = prompt("Who identified the bug?");
-    let date = prompt("What is on which you discovered the bug?");
-    let project = prompt("What project is this bug related to?");
-    let personAssigned = prompt("Please enter your name.");
-    let statusOfIssue = alert("The status of the issue is: ");
-
-        let issues = new issue(`${summary}`, `${desc}`, `${personID}`, `${date}`, `${project}`, `${personAssigned}`, `${statusOfIssue}`);
-
-        issueArr.push(issues);
 
 issue();
 
@@ -36,6 +22,6 @@ issue();
 //assigning issues
 const defPerson = ["Ashley V.", "Chandri B.", "Kelo L.", "Peter S."];
 
-for (let is of issueArr){
+for (let is of issueArr) {
     alert(`Issue ${issueArr[is]} is assigned to ${defPerson[i]}`);
 }
